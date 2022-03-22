72294ade05ba0af2242604c826023a8221a5c6c3
valeria <valeria.granada@evertecinc.com>
2022-03-22 01:11:49 -0500

    WIP: apis

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'user_name' => $this->user->name,
+            'user_email' => $this->user->email,
+            'created_at' => $this->created_at->format('Y-m-d'),
+            'updated_at' => $this->updated_at->format('Y-m-d'),
+        ];
+    }
+}
